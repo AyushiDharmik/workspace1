629f4c711ad0feb54c0be19cf9013ee9b244a61f
Iamneo <techadmin@iamneo.ai>
2023-11-11 04:18:41 +0000

    Backup Commit

--- AngularApp/src/app/crud/crud.component.ts
+++ AngularApp/src/app/crud/crud.component.ts
@@ -5,41 +5,44 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './crud.component.html',
   styleUrls: ['./crud.component.css']
 })
+
 export class CrudComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
-    
-  
+
   }
-  
-indiData:object={email:" ",password:" ",isReady:false};
-  userData:object[]=[{}];
- 
-  dataHandler(event){
-    let ids:string = event.target.id;
-    let data:any =event.target.value;
- 
-    switch (ids)
-    {
+
+  indiData: object = { email: " ", password: " ", isReady: false };
+  userData: object[] = [{}];
+
+  dataHandler(event) {
+    let ids: string = event.target.id;
+    let data: any = event.target.value;
+
+    switch (ids) {
       case "EMID":
-        this.indiData={...this.indiData,email:data};
+        this.indiData = { ...this.indiData, email: data };
         break;
+
       case "PSID":
-        this.indiData={...this.indiData,password:data};
+        this.indiData = { ...this.indiData, password: data };
         break;
+
       case "CHID":
-        this.indiData={...this.indiData,isReady:data};
+        this.indiData = { ...this.indiData, isReady: data };
         break;
+
       case "btn1":
         console.log(this.indiData);
         this.userData.push(this.indiData);
         console.log(this.userData);
         break;
+        
       default:
         break;
-     
+
     }
     console.log(event.target.id);
   }
