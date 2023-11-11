ca7a58db24ba6a1d4658e04efe22363ab0282f1c
Iamneo <techadmin@iamneo.ai>
2023-11-11 08:50:51 +0000

    Backup Commit

--- AngularApp/src/app/crud/crud.component.ts
+++ AngularApp/src/app/crud/crud.component.ts
@@ -14,52 +14,54 @@ export class CrudComponent implements OnInit {
 
   }
   date=new Date();
-  indiData: object = { email: " ", password: " ", isReady: false };
-  userData: object[] = [{}];
+  money:number=10000;
+  
+  // indiData: object = { email: " ", password: " ", isReady: false };
+  // userData: object[] = [{}];
 
-  dataHandler(event) {
-    let ids: string = event.target.id;
-    let data: any = event.target.value;
+  // dataHandler(event) {
+  //   let ids: string = event.target.id;
+  //   let data: any = event.target.value;
 
-    switch (ids) {
-      case "EMID":
-        this.indiData = { ...this.indiData, email: data };
-        if(this.userData.length!=0)
-        {
-          console.log("working..")
-          let value=this.userData.find(x=>x.email==this.indiData.email);
-          this.indiData={...value,password:value.password,isReady:value.isReady}
-          console.log(value);
-        }
-        break;
+  //   switch (ids) {
+  //     case "EMID":
+  //       this.indiData = { ...this.indiData, email: data };
+  //       if(this.userData.length!=0)
+  //       {
+  //         console.log("working..")
+  //         let value=this.userData.find(x=>x.email==this.indiData.email);
+  //         this.indiData={...value,password:value.password,isReady:value.isReady}
+  //         console.log(value);
+  //       }
+  //       break;
 
-      case "PSID":
-        this.indiData = { ...this.indiData, password: data };
-        break;
+  //     case "PSID":
+  //       this.indiData = { ...this.indiData, password: data };
+  //       break;
 
-      case "CHID":
-        let chkVal=event.target.checked;
-        this.indiData = { ...this.indiData, isReady: chkVal };
-        break;
+  //     case "CHID":
+  //       let chkVal=event.target.checked;
+  //       this.indiData = { ...this.indiData, isReady: chkVal };
+  //       break;
 
-      case "btn1":
-        console.log(this.indiData);
-        this.userData.push(this.indiData);
-        console.log(this.userData);
-        break;
+  //     case "btn1":
+  //       console.log(this.indiData);
+  //       this.userData.push(this.indiData);
+  //       console.log(this.userData);
+  //       break;
 
-      case "btn2":
-          let value=this.userData.find(x=>x.email==this .indiData.email);
-          console.log(value)
-          this.userData.push(this.indiData);
+  //     case "btn2":
+  //         let value=this.userData.find(x=>x.email==this .indiData.email);
+  //         console.log(value)
+  //         this.userData.push(this.indiData);
 
-          break;
+  //         break;
 
-      default:
-        break;
+  //     default:
+  //       break;
 
-    }
-    console.log(event.target.id);
-  }
+  //   }
+  //   console.log(event.target.id);
+  // }
 
 }
