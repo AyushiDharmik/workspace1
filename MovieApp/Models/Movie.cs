455679228a444cb9b316d1578042f661401c44a1
Iamneo <techadmin@iamneo.ai>
2023-11-17 16:25:56 +0000

    Backup Commit

---
+++ MovieApp/Models/Movie.cs
@@ -0,0 +1,16 @@
+movie class:using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+ 
+namespace MovieApp.Models;
+    public class Movie
+    {
+        [Key]
+       public int Id{get; set;}
+       [Required]
+      public string? Name {get; set;}
+      public int YearRelease {get; set;}
+      public int Rating{get; set;}
+ 
+      public ICollection<Detail> ? Details{get; set;}
+    }
