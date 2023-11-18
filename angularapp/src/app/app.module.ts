9fb1568609df0b5dc3920d32883c8c782d9972d3
Iamneo <techadmin@iamneo.ai>
2023-11-18 09:26:08 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClient,
+    HttpClientModule
+    
   ],
-  providers: [],
+  providers: [HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
---
+++ angularapp/src/app/model/iemployee.ts
@@ -0,0 +1,6 @@
+export interface IEmployee {
+    employeeId:number;
+    employeeName:string;
+    departmentId:number;
+    salary:number;
+}
