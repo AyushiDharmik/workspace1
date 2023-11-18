a5b8f82c97adce1ba2b7750319f037a4fce489e6
Iamneo <techadmin@iamneo.ai>
2023-11-18 10:11:07 +0000

    Backup Commit

--- angularapp/src/app/get-dept/get-dept.component.ts
+++ angularapp/src/app/get-dept/get-dept.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { IDepartment } from '../model/idepartment';
-import { HttpClient } from '@angular/common/http';
 import { CrudService } from '../service/crud.service';
 
 
