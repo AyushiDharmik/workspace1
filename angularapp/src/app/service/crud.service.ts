19a7373138836d8debac473b127b3573a4901b83
Iamneo <techadmin@iamneo.ai>
2023-11-18 09:41:07 +0000

    Backup Commit

--- angularapp/src/app/service/crud.service.ts
+++ angularapp/src/app/service/crud.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClientModule,HttpHeaders, HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IDepartment } from '../model/idepartment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService {
 
-  constructor() { }
+
+  private url = "https://8080-cabbaacddbbbfabcaaaceeafebecebbffdafdefabcc.premiumproject.examly.io/api/Department"
+  constructor(private httpclient: HttpClient) { }
+
+  // httpOptions = { headers: new HttpHeaders({ 'Content-type': 'application/json' }) }
+
+  getAllDept(): Observable<IDepartment> {
+    return this.httpclient.get<IDepartment>(this.url )
+  }
 }
