ff6ecdd3ba81f79fa9a7b4d988b0361c6b60ceed
Iamneo <techadmin@iamneo.ai>
2023-11-20 05:10:55 +0000

    Backup Commit

--- angularapp/src/app/edit-emp/edit-emp.component.ts
+++ angularapp/src/app/edit-emp/edit-emp.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { IEmployee } from '../model/iemployee';
+import { CrudService } from '../service/crud.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-emp',
@@ -6,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-emp.component.css']
 })
 export class EditEmpComponent implements OnInit {
+ employeeId:number
+ empData:IEmployee={
+   employeeId: 0,
+   employeeName: '',
+   departmentId: 0,
+   salary: 0
+ }
 
-  constructor() { }
+  constructor(private cs:CrudService, private route:Router, private ar:ActivatedRoute) { }
 
   ngOnInit() {
+    const tid=this.ar.snapshot.paramMap.get('employeeId')
+    this.employeeId=Number(tid)
+    this.cs.FindId(this.employeeId).subscribe((data:IEmployee)=>this.empData=data)
+  }
+
+  editData(userData:NgForm)
+  {
+    let empData:IEmployee={
+      employeeId: this.empData.employeeId,
+      employeeName:userData.controls['ename'].value,
+      departmentId:userData.controls['dId'].value,
+      salary:userData.controls['salary'].value
+    }
+    this.cs.EditDept(empData).subscribe(()=>
+      this.route.navigate(["/DisplayEmp"]))
   }
 
 }
