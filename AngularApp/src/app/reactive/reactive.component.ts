1a51bd4acf9fc8ae1fb414f04f0c58c4ac872331
Iamneo <techadmin@iamneo.ai>
2023-11-25 08:15:09 +0000

    Backup Commit

--- AngularApp/src/app/reactive/reactive.component.ts
+++ AngularApp/src/app/reactive/reactive.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveComponent implements OnInit {
 
-  constructor() { }
+  error:string = "";
+   demoForm:any;
+   constructor(private formBuilder:FormBuilder){
+    this.demoForm = this.formBuilder.group({
+      email:['',Validators.email],
+      password:['',Validators.required],
+      opt:['Select',Validators.required],
+      gender:['',Validators.required],
+      user:['',Validators.required],
+      chkValue:[false],
+      startDate:['',Validators.required]
 
-  ngOnInit() {
+
+    })
+   }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
+  
+   display(){
+      console.log(this.demoForm.value);
+      this.demoForm.get('user').errors;
+      console.log(this.demoForm.valid);
+   }
 
 }
