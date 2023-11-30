7c1b196e2553edbfd74e39f32b29b4c7f91c8265
Iamneo <techadmin@iamneo.ai>
2023-11-30 09:33:17 +0000

    Backup Commit

--- CourseApi/Startup.cs
+++ CourseApi/Startup.cs
@@ -7,6 +7,52 @@ namespace CourseApi
 {
     public class Startup
     {
-        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDistributedMemoryCache();
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(100);
+            });
+            services.AddMvc();
+        }
     }
+
+    public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
+    {
+
+        if (env.IsDevelopment())
+
+        {
+
+            app.UseDeveloperExceptionPage();
+
+            app.UseBrowserLink();
+
+        }
+
+        else
+
+        {
+
+        }
+
+        app.UseExceptionHandler("/Home/Error");
+
+        app.UseStaticFiles();
+
+        app.UseSession();
+
+        app.UseMvc(routes =>
+
+        {
+
+            routes.MapRoute(
+
+    name: "default", template: "{controller=Home}/{action=Index}/{id?}");
+
+        });
+
+    }
+
 }
